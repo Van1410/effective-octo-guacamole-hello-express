c3593aa705749dba787a06baf1991e345d444ca6
Van1410 <vhamlin@una.edu>
2025-08-26 20:01:24 -0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,9 +1,30 @@
 import express from 'express'
+import path from 'path';
 
 const app = express()
+const PORT = process.env.PORT || 3000; 
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello Express  . <a href="Van">Van</a>')
 })
 
-app.listen(3000)
+// endpoints...middlewares...apis? 
+// send an html file
+
+app.get('/barry', (req, res) => {
+  // res.send('barry. <a href="/">home</a>')
+
+  res.sendFile('Van.html'); 
+
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
