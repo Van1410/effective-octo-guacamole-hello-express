78434730700db6b1fd4ba7ae5b70bdd963c66334
Hamlin <vhamlin@una.edu>
2025-08-28 13:39:10 -0500

    iss3

--- app.mjs
+++ app.mjs
@@ -1,9 +1,35 @@
+//imports go up top
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import express from 'express'
 
-const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express()
+const PORT = process.env.PORT || 3000; 
+
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/barry', (req, res) => {
+  res.sendFile(join(__dirname, 'public', 'barry.html')) 
 })
 
-app.listen(3000)
+app.get('/', (req, res) => {
+  res.send('Hello Express from Render 😍😍😍. <a href="barry">barry</a>')
+})
+
+
+app.get('/barry', (req, res) => {
+  res.sendFile(join(__dirname, 'public', 'barry.html')); 
+})
+
+app.get('/api/barry', (req, res) => {
+  // res.send('barry. <a href="/">home</a>')
+  const myVar = 'Hello from server!';
+  res.json({ myVar });
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
