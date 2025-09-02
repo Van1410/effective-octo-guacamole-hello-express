79af0f5c1c7e561e8bbdc05c86bac4ae3f897d55
Van1410 <vhamlin@una.edu>
2025-09-02 17:41:29 -0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,27 +1,30 @@
-//imports go up top
+import express from 'express'
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import express from 'express'
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const app = express()
 const PORT = process.env.PORT || 3000; 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(join(__dirname, 'public')));
 
-app.get('/barry', (req, res) => {
-  res.sendFile(join(__dirname, 'public', 'barry.html')) 
-})
+app.use(express.json()); 
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello Express from Render 😍😍😍. <a href="barry">barry</a>')
 })
 
+// endpoints...middlewares...apis? 
 
+// send an html file
 app.get('/barry', (req, res) => {
-  res.sendFile(join(__dirname, 'public', 'barry.html')); 
+ 
+  res.sendFile(join(__dirname, 'public', 'barry.html')) 
+
 })
 
 app.get('/api/barry', (req, res) => {
@@ -29,6 +32,35 @@ app.get('/api/barry', (req, res) => {
   const myVar = 'Hello from server!';
   res.json({ myVar });
 })
+
+app.get('/api/query', (req, res) => {
+
+  //console.log("client request with query param:", req.query.name); 
+  const name = req.query.name; 
+  res.json({"message": `Hi, ${name}. How are you?`});
+
+  // receivedData.queries.push(req.query.name || 'Guest');
+  // const name = req.query.name || 'Guest';
+  // res.json({ message: `Hello, ${name} (from query param)` });
+});
+
+app.get('/api/url/:iaddasfsd', (req, res) => {
+
+  console.log("client request with URL param:", req.params.iaddasfsd); 
+  // const name = req.query.name; 
+  // res.json({"message": `Hi, ${name}. How are you?`});
+
+});
+
+
+app.get('/api/body', (req, res) => {
+
+  console.log("client request with POST body:", req.query); 
+  // const name = req.body.name; 
+  // res.json({"message": `Hi, ${name}. How are you?`});
+
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
