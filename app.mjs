2f69c3148af38491c120203371b02b0d2976aec7
Van1410 <vhamlin@una.edu>
2025-09-03 10:52:54 -0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,59 +1,54 @@
-import express from 'express'
+// app.mjs (or app.js with "type": "module")
+import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const app = express()
-const PORT = process.env.PORT || 3000; 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const app = express()
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(join(__dirname, 'public')));
-
-app.use(express.json()); 
-
-
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello Express from Render 😍😍😍. <a href="barry">barry</a>')
+  res.send('Hello World from Render <a href="/barry">barry</a>')
 })
 
-// endpoints
-
+// endpoints ... middlewares ... apis?
 // send an html file
-app.get('/barry', (req, res) => {
- 
-  res.sendFile(join(__dirname, 'public', 'barry.html')) 
 
+app.get('/barry', (req, res) => {
+  res.sendFile(join(__dirname, 'public', 'barry.html')) 
 })
 
 app.get('/api/barry', (req, res) => {
-
+  // res.send('barry. <a href="/">home</a>')
   const myVar = 'Hello from server!';
   res.json({ myVar });
 })
 
+//app.listen(3000)
+
 app.get('/api/query', (req, res) => {
 
-  const name = req.query.name; 
-  res.json({"message": `Hi, ${name}. How are you?`});
+  //console.log('Client request with query param:', req.query.name);
+  const name = req.query.name;
+  res.json({
+    "message": name
+  })
+})
 
-});
+app.get('/api/url/:id', (req, res) => {
+  console.log('Client request with url param:', req.params.id);
+})
 
-app.get('/api/url/:iaddasfsd', (req, res) => {
+app.post('/api/body', (req, res) => {
 
-  console.log("client request with URL param:", req.params.iaddasfsd); 
- 
+  console.log('Client request with POST name:', req.body.name);
+  console.log('Client request with POST zip:', req.body.zip);
 
-});
-
-
-app.get('/api/body', (req, res) => {
-
-  console.log("client request with POST body:", req.query); 
-  
-});
-
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
